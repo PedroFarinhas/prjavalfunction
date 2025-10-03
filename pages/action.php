af608cdf564551fa07ceb138d36070ebb392412d
Pedro <pedro.gfainhas@senacsp.edu.br>
2025-10-03 20:52:18 -0300

    criação da página php sem functions mas funcional

--- pages/action.php
+++ pages/action.php
@@ -1,9 +1,34 @@
 <?php
-$valor = $_GET["valor"];
-$valor = number_format($valor,2,",",".");
+$value = $_GET["valor"];
+$conversao = 0;
+$euro = 0.16;
+$dollar = 0.19;
+$dinar = 0.057;
 
-
-
+if (!isset($_GET['moeda']) || filter_var($value, FILTER_VALIDATE_INT) == false) {
+    $mensagem = "Selecione uma opção e digite os números corretamente";
+} else {
+    $moeda = $_GET['moeda'];
+    if ($moeda == "Euro") {
+        $conversao = ($value * $euro);
+        $sigla = " €: ";
+        $taxa = $euro;
+    }
+    if ($moeda == "Dollar") {
+        $conversao = $value * $dollar;
+        $sigla = " $: ";
+        $taxa = $dollar;
+    }
+    if ($moeda == "Dinarkuwaitiano") {
+        $conversao = $value * $dinar;
+        $moeda = "Dinar Kuwaitiano";
+        $sigla = " KWD: ";
+        $taxa = $dinar;
+    }
+    $value = number_format($_GET["valor"], 2, ",", ".");
+    $conversao = number_format($conversao, 2, ",", ".");
+    $mensagem = "Você selecionou " . $moeda . ", a taxa de conversão é de " . $taxa . ", o valor para conversão foi de R$: " . $value .  ".  Convertendo ficou " . $sigla . $conversao. ".";
+}
 ?>
 
 <!DOCTYPE html>
@@ -12,19 +37,13 @@ $valor = number_format($valor,2,",",".");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VALOR CONVERTIDO</title>
-    <link rel="stylesheet" href="style.css">
-
+    <link rel="stylesheet" href="../style/style.css">
 </head>
 <body>
-
-    <div style= " display: inline-block;  border: 2px solid black; margin: 10px; padding: 10px;">
-
+    <div style=" display: inline-block;  border: 2px solid black; margin: 10px; padding: 10px;">
         <p>
-            <?= "O valor convertido é: {$valor}"  ?>
-
+            <?= "{$mensagem}"  ?>
         </p>
-
     </div>
-    
 </body>
 </html>
